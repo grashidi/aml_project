c318b801ed1e2a2946f62b0dd5b23a72ea2ec78c
gabi <>
2021-08-05 16:01:16 +0200

    added test routine

--- code/train_util.py
+++ code/train_util.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from sklearn.metrics import roc_auc_score
+from tqdm import tqdm
 
 
 def fit(model, optimizer, scheduler, criterion, train_loader, val_loader, epochs):
@@ -91,15 +92,15 @@ def validate(model, criterion, val_loader, device):
         # list containing prediction of the model
         # 0 for covid negative prediction
         # 1 for covid positive prediction
-        predlist = []
+        pred_list = []
 
         # list containing the true values
         # 0 for covid negative
         # 1 for covid positive
-        targetlist = []
+        target_list = []
 
         # list containing the softmax values of the predicted logits
-        scorelist = []
+        score_list = []
 
 
         # Predict
@@ -115,26 +116,103 @@ def validate(model, criterion, val_loader, device):
             correct += pred.eq(labels.long().view_as(pred)).sum().item()
 
             targetcpu = [l.item() for l in labels.long().cpu()]
-            predlist += [p.item() for p in pred.cpu()]
-            scorelist += [s.item() for s in score.cpu()[:,1]]
-            targetlist += targetcpu
+            pred_list += [p.item() for p in pred.cpu()]
+            score_list += [s.item() for s in score.cpu()[:,1]]
+            target_list += targetcpu
 
-    return targetlist, scorelist, predlist, (val_loss / len(val_loader))
+    return target_list, score_list, pred_list, (val_loss / len(val_loader))
 
 
-def compute_statistics(vote_pred, vote_score, targetlist):
-    vote_pred = np.asarray(vote_pred)
-    vote_score = np.asarray(vote_score)
-    targetlist = np.asarray(targetlist)
+def test(model, criterion, test_loader):
+    device = "cuda"
 
-    TP = ((vote_pred == 1) & (targetlist == 1)).sum()
-    TN = ((vote_pred == 0) & (targetlist == 0)).sum()
-    FN = ((vote_pred == 0) & (targetlist == 1)).sum()
-    FP = ((vote_pred == 1) & (targetlist == 0)).sum()
+    # Check out available devices
+    device_count = torch.cuda.device_count()
+    if device_count > 1:
+        print("Devices in use:")
+        for dc in device_count:
+            print(torch.cuda.get_device_name(device=dc))
+        model = nn.DataParallel(model)
+    elif device_count == 1:
+        print("Device in use:")
+        print(torch.cuda.get_device_name(device=0))
+    else:
+        print("No acclerator available. Using CPU.")
+        device = "cpu"
 
-    precision = TP / (TP + FP)
-    recall = TP / (TP + FN)
+    model.eval()
+    model.to(device)
+
+    test_loss = 0
+    correct = 0
+
+    TP = 0 # true positive
+    TN = 0 # true negative
+    FN = 0 # false negative
+    FP = 0 # false positive
+
+    # Don't update model
+    with torch.no_grad():
+        # list containing prediction of the model
+        # 0 for covid negative prediction
+        # 1 for covid positive prediction
+        pred_list = []
+
+        # list containing the true values
+        # 0 for covid negative
+        # 1 for covid positive
+        target_list = []
+
+        # list containing the softmax values of the predicted logits
+        score_list = []
+
+
+        # Predict
+        print("Testing ...")
+        for batch_samples in tqdm(test_loader):
+            images, labels = batch_samples['img'].to(device), batch_samples['label'].to(device)
+
+            output = model(images)
+
+            test_loss += criterion(output, labels.long())
+            score = F.softmax(output, dim=1)
+            pred = output.argmax(dim=1, keepdim=True)
+
+            correct += pred.eq(labels.long().view_as(pred)).sum().item()
+
+            targetcpu = [l.item() for l in labels.long().cpu()]
+            pred_list += [p.item() for p in pred.cpu()]
+            score_list += [s.item() for s in score.cpu()[:,1]]
+            target_list += targetcpu
+
+        precision, recall, acc, auc = compute_statistics(pred_list, score_list, target_list)
+
+        print(("test_loss: %.3f\t"
+               "precision: %.3f\t"
+               "recall: %.3f\t"
+               "accuracy: %.3f\t"
+               "AUC: %.3f") %
+              (test_loss / len(test_loader),
+               precision,
+               recall,
+               acc,
+               auc))
+
+
+
+def compute_statistics(pred_list, score_list, target_list):
+    pred_list = np.asarray(pred_list)
+    score_list = np.asarray(score_list)
+    target_list = np.asarray(target_list)
+
+    TP = ((pred_list == 1) & (target_list == 1)).sum()
+    TN = ((pred_list == 0) & (target_list == 0)).sum()
+    FN = ((pred_list == 0) & (target_list == 1)).sum()
+    FP = ((pred_list == 1) & (target_list == 0)).sum()
+
+    precision = (TP / (TP + FP)) if (TP + FP) > 0. else 0.
+    recall = (TP / (TP + FN)) if (TP + FN) > 0. else 0.
     acc = (TP + TN) / (TP + TN + FP + FN)
-    auc = roc_auc_score(targetlist, vote_score)
+    auc = roc_auc_score(target_list, score_list)
 
     return precision, recall, acc, auc
