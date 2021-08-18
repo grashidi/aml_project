2608f76e847c37a1b5a945d1d6f74d2c4fab68ed
gabi <>
2021-08-18 17:09:34 +0200

    handle package not found error

--- code/install.sh
+++ code/install.sh
@@ -4,9 +4,16 @@ python3 -m venv env
 
 source env/bin/activate
 
+python -m pip install -U pip
+
 while IFS= read -r package; do
-     pip3 install $package
+     {
+	pip3 install $package
+     } || {
+	echo "INSTALLING ALTERNATIVE PACKAGE..."
+	unspecified_package=($(echo $package | tr "==" " "))
+	pip3 install ${unspecified_package[0]}
+     }
 done < requirements.txt
 
 python -m pip install --editable ./
-
