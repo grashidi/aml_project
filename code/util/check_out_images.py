d194c26d027fca25af5bfa81490d12d201d95066
gabi <>
2021-09-20 10:02:10 +0200

    added function for image plotting

---
+++ code/util/check_out_images.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+def check_out_images(data_loader, num_images):
+    """
+    Plots num_images of the given dataloader.
+
+    Args:
+        data_loader (DataLoader class): Data loader containing x-rays or CT-scans
+        num_images: (int): Number of images to be plotted.
+    """
+    for batch_index, batch_samples in enumerate(data_loader):
+        im, labels = batch_samples['img'], batch_samples['label']
+        c1 = plt.imshow(im[0,0,:,:].numpy(), alpha=1.0)
+        plt.colorbar(c1)
+        plt.savefig("test_" + str(batch_index) + ".png")
+
+        if batch_index > (num_images - 2):
+            break
