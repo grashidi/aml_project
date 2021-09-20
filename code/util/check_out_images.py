3046a40e1dc3a08a7cedb8672d2a0e1f3316a50d
gabi <>
2021-09-20 10:27:37 +0200

    commenting

--- code/util/check_out_images.py
+++ code/util/check_out_images.py
@@ -5,7 +5,7 @@ def check_out_images_resnet(data_loader, num_images=20):
     Plots num_images of the given dataloader.
 
     Args:
-        data_loader (DataLoader class): Data loader containing x-rays or CT-scans
+        data_loader (DataLoader class): Dataloader containing x-rays or CT-scans
         num_images: (int): Number of images to be plotted.
     """
     for batch_index, batch_samples in enumerate(data_loader):
@@ -14,6 +14,29 @@ def check_out_images_resnet(data_loader, num_images=20):
         c1 = plt.imshow(im[0,0,:,:].numpy(), alpha=1.0)
         plt.colorbar(c1)
         plt.savefig("test_" + str(batch_index) + ".png")
+
+        if batch_index > (num_images - 2):
+            break
+
+
+def check_out_images_unet(data_loader, num_images=20):
+    """
+    Plots num_images of the given dataloader.
+
+    Args:
+        data_loader (DataLoader class): Dataloader containing x-rays or CT-scans
+        num_images: (int): Number of images to be plotted.
+    """
+    for batch_index, batch_samples in enumerate(data_loader):
+        im, labels = batch_samples['img'], batch_samples['label']
+        plt.figure()
+        c1 = plt.imshow(im[0,0,:,:].numpy(), alpha=1.0)
+        plt.colorbar(c1)
+        plt.savefig("test_" + str(batch_index) + "_im.png")
+        plt.figure()
+        c2 = plt.imshow(labels[0,0,:,:].numpy(), alpha=1.0)
+        plt.colorbar(c2)
+        plt.savefig("test_" + str(batch_index) + "_label.png")
 
         if batch_index > (num_images - 2):
             break
