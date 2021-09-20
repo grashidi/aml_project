00794bbb9d424bf6f0d8aa71044ea22596cd58c2
gabi <>
2021-09-20 09:41:50 +0200

    added flag for overlay + commenting

--- code/util/covid_dataset.py
+++ code/util/covid_dataset.py
@@ -17,7 +17,8 @@ def read_txt(txt_path):
 
 
 class CovidDataset(Dataset):
-    def __init__(self, root_dir, txt_COVID, txt_NonCOVID, unet=None, train=False, p=0.5, use_cache=False):
+    def __init__(self, root_dir, txt_COVID, txt_NonCOVID, train=False, unet=None, overlay=False,
+                 p=0.5, use_cache=False):
         """
         Args:
             root_dir (string): Directory with all the images.
@@ -26,6 +27,9 @@ class CovidDataset(Dataset):
             txt_COVID (string): Path to the txt file containing the datasplit's COVID positive image paths.
             txt_NonCOVID (string): Path to the txt file containing the datasplit's COVID negative image paths.
             train (bool): Apply training transforms if true.
+            unet (class UNet): Trained UNet for lung segmentation.
+            overlay (bool): If true overlay ROI mask will be applied. Otherwise last image's last channel
+                            will be replaced with ROI mask.
             p (float): Probability for data augmentations.
             use_cache (bool): Cache data in main memory before starting the training.
         """
@@ -34,6 +38,7 @@ class CovidDataset(Dataset):
         self.classes = ['NonCOVID', 'COVID']
         self.num_cls = len(self.classes)
         self.img_list = []
+        self.overlay = overlay
         self.use_cache = use_cache
         self.p = p
         self.cached_data = []
@@ -83,7 +88,7 @@ class CovidDataset(Dataset):
 
     def get_train_transforms(self):
         """
-        Creates composed transformation function for training data
+        Creates composed transformation function for training data.
 
         Returns:
             test_transform (ComposeDouble): Composed function performing
@@ -105,7 +110,7 @@ class CovidDataset(Dataset):
 
     def get_test_transforms(self):
         """
-        Creates composed transformation function for test/validation data
+        Creates composed transformation function for test/validation data.
 
         Returns:
             test_transform (ComposeDouble): Composed function performing
@@ -126,10 +131,10 @@ class CovidDataset(Dataset):
         Rotate by one of the defined angles with probability p.
 
         Args:
-            x (tensor): Tensor to be rotated
+            x (tensor): Tensor to be rotated.
 
         Returns:
-            A (tensor): Rotated tensor
+            A (tensor): Rotated tensor.
         """
         angles = [90, 180, 270]
 
@@ -140,13 +145,15 @@ class CovidDataset(Dataset):
 
     def mask_transform(self, x):
         """
-        Set pixels outside of mask to zero
+        Set pixels outside of mask to zero.
+        Overlay: set pixels outside of mask to min value.
+        Replace channel: replace last channel with mask values.
 
         Args:
-            x (tensor): Tensor to be masked
+            x (tensor): Tensor to be masked.
 
         Returns:
-            x (tensor): Masked tensor
+            x (tensor): Masked tensor.
         """
         if self.unet:
             threshold = 0.5
@@ -155,19 +162,21 @@ class CovidDataset(Dataset):
             zero = torch.zeros_like(mask, dtype=torch.long)
             one = torch.ones_like(mask, dtype=torch.long)
             mask = torch.where(mask >= threshold, one, zero)
-            x[-1,:,:] = mask[0,0,:,:] # replace last channel with mask
-            # x[mask[0,:,:,:].repeat(3,1,1) == 0] = torch.min(x) # set pixels outside of mask to min value
+            if self.overlay:
+                x[mask[0,:,:,:].repeat(3,1,1) == 0] = torch.min(x)
+            else:
+                x[-1,:,:] = mask[0,0,:,:]
         return x
 
     def normalize(self, A):
         """
-        Normalize tensor with precomputed (training dataset) mean and std
+        Normalize tensor with precomputed (training dataset) mean and std.
 
         Args:
-            A (tensor): Tensor to be normalized
+            A (tensor): Tensor to be normalized.
 
         Returns:
-            A (tensor): Normalized tensor
+            A (tensor): Normalized tensor.
         """
         _normalize = transforms.Normalize(mean=[0.2099, 0.2098, 0.2098],
                                           std=[0.7148, 0.7148, 0.7148])
@@ -176,13 +185,13 @@ class CovidDataset(Dataset):
 
     def normalize_to_range_0_1(self, A):
         """
-        Normalize array to values within range [0, 1]
+        Normalize array to values within range [0, 1].
 
         Args:
-            A (tensor): Tensor with values in range [0, 255]
+            A (tensor): Tensor with values in range [0, 255].
 
         Returns:
-            B (tensor): Tensor with values in range [0, 1]
+            B (tensor): Tensor with values in range [0, 1].
         """
         B = A - A.min()
         B /= B.max()
